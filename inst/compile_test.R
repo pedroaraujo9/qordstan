f711a33b81c2721788954e7a7a87087887e87e54
Pedro Araújo <pedromenezes98@gmail.com>
2021-09-29 22:22:14 -0300

    unique file to gen_data_example

--- inst/compile_test.R
+++ inst/compile_test.R
@@ -1,12 +1,16 @@
 roxygen2::roxygenize()
 devtools::install()
 
+library(tidyverse)
 library(qordstan)
+library(rstan)
 
 data = gen_data_example()
 fit = qord_fit(data$x, data$y, p = 0.5)
 
-fit
+
+am = fit %>% rstan::extract()
+am$gammas %>% colMeans()
 data$b
 data$deltas
 data$gammas
